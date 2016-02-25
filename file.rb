05cadeb48fba14b6612b3f3432c9f786f3f6e15a
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-02-25 10:59:03 +0530

    [FEAT] file conflicts removed

--- file.rb
+++ file.rb
@@ -1,3 +1,4 @@
+
 require 'fileutils' #for moving files across directories
 
 # arguments checking
@@ -22,10 +23,41 @@ main_list.each{ |m| m.delete! '.'}
 main_list.each do |f| 	 
 	Dir.mkdir(File.join(Dir.pwd, f),0775) unless dir_list.include?(f)
 end
+
+# file moving and cheking if the file exists
 file_list.each do |f|
 	fol = (File.extname f).to_s
 	fol.delete! '.'
-	FileUtils.mv(f,fol+"/"+f);
+	if Dir.entries(fol).include?(f) 
+		puts "file name #{f} exist"
+		puts "1 to rename"
+		puts "2 to overwrite"
+		puts "3 to ignore"
+		inp = STDIN.gets.chomp.to_i	
+		case inp
+		when 1
+			fname = f
+			while true
+				puts "enter file name:"
+				fname =  STDIN.gets.chomp
+				puts (File.extname fname).to_s
+				fname = fname +"."+ fol if (File.extname fname).to_s.empty? 
+				if Dir.entries(fol).include?(fname)
+					puts "name exist try another"
+				else 
+					break
+				end
+			end
+			FileUtils.mv(f,fol+"/"+fname)
+		when 2
+			FileUtils.mv(f,fol+"/"+f)
+		when 3
+			next
+		else 
+			puts "invalid input try again"
+			redo
+		end
+	else
+		FileUtils.mv(f,fol+"/"+f)
+	end
 end
-
-
